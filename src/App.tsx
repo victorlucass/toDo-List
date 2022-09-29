f415e07ad977d940de44e6e4fcc6402b70cbd7a3
Victor Lucas A. de Oliveira <59880314+victorlucass@users.noreply.github.com>
2022-09-28 23:45:33 -0400

    initial project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Header } from "./components/Header";
+import "./styles/global.scss";
+import "./styles/reset.scss";
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
